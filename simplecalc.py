d89fce11b597c77b117dfa577cdb29d7f332ad02
JC <juan.romero.204@mi.unc.edu.ar>
2025-06-05 00:12:28 -0300

    Update simplecalc.py

--- simplecalc.py
+++ simplecalc.py
@@ -1,23 +1,24 @@
+# The addition function
 def addition (number1 , number2):
     return number1 + number2
-
+# The substraction function
 def substraction (number1, number2):
     return number1 - number2
-
+#The multiplication function
 def multiplication(number1, number2):
     return number1 * number2
-
+#The division function
 def division(number1, number2):
     return number1 / number2
-
+# Now it's time to define the calculator function
 def calculator(operation, number1 , number2):
     return operation(number1 , number2)
-
+# Establishing variables and asking the user for input
 number1 = int(input("What's the value of the first number? : "))
 number2 = int(input("What's the value of the second number? : "))
 operation = int(input("What would you like me to do? press 0 for addition , 1 for substraction , 2 for multiplication and 3 for division : "))
 
-
+# The conditionals for the operations 
 if operation == 0 :
     print(calculator(addition , number1 , number2))
 
